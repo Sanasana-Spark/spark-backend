ef8426b96b773dc2720867e2271b0c4e59a1de99
perismuthoni <muthonimuriuki22@gmail.com>
2024-10-01 16:21:57 +0300

    get asset summaries

--- sanasana/models/assets.py
+++ sanasana/models/assets.py
@@ -62,4 +62,18 @@ def get_asset_by_id(trip_id):
         id=trip_id
     ).first()
     return act
-    
+
+
+def get_asset_count_by_org(org_id):
+    count_of_assets = db.session.query(
+        func.count(Asset.id)  # Replace Asset.id with the appropriate column if needed
+    ).filter(Asset.a_organisation_id == org_id).scalar()
+
+    return count_of_assets
+
+def get_asset_value_sum_by_org(org_id):
+    sum_of_values = db.session.query(
+        func.sum(Asset.a_value)
+    ).filter(Asset.a_organisation_id == org_id).scalar()
+
+    return sum_of_values
